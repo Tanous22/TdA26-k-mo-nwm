d2cfda6ab1913d9ef5149ada3bb99b3b1e481c6f
Tanous22 <rulikda@eskola.eu>
2025-11-10 17:59:53 +0100

    Initial commit

---
+++ apps/web/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const users = ref<unknown>(null)
+
+onMounted(async () => {
+  const response = await fetch('/api/users')
+  users.value = await response.json()
+})
+</script>
+
+<template>
+  <div>
+    <pre>
+      {{ JSON.stringify(users, null, 2) }}
+    </pre>
+    <div>
+      <section>
+        <h2>API Status - vue</h2>
+      </section>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
