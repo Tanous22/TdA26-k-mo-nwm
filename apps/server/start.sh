7006075ef9b82fb52e14621912725ea12c1aaeea
Tanous22 <rulikda@eskola.eu>
2025-11-13 22:50:24 +0100

    Change MySQL host from localhost to mysql

--- apps/server/start.sh
+++ apps/server/start.sh
@@ -4,11 +4,12 @@ set -e
 echo "Waiting for MySQL to be ready..."
 
 # Wait for MySQL to be available
-until nc -z localhost 3306; do
+until nc -z mysql 3306; do
   echo "MySQL is unavailable - sleeping"
   sleep 2
 done
 
 echo "MySQL is up - starting server"
 npm start
+
 
