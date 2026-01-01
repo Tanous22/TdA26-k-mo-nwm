20ca9d302eebe5f365c27cced2fa03b122efc456
Tanous22 <rulikda@eskola.eu>
2026-01-01 22:35:06 +0100

    Delete lockin/TdA26-k-mo-nwm directory

--- lockin/TdA26-k-mo-nwm/apps/server/src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2/promise";
-
-export const pool = mysql.createPool({
-	uri: process.env.DATABASE_URL,
-	waitForConnections: true,
-	connectionLimit: 10,
-	queueLimit: 0,
-});
